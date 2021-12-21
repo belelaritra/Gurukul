5e66ede24795e3bd75ee17732cd6e61fbb48baf6
belelaritra <belelaritra@gmail.com>
2021-12-22 01:58:54 +0530

    Modified : Navbar , Login & Signup Page
    Navbar , Login & Signup Page Modified.

--- Account/models.py
+++ Account/models.py
@@ -7,7 +7,7 @@ class Profile(models.Model):
     fname = models.CharField(max_length=100, default="")
     lname = models.CharField(max_length=100, default="")
     email = models.EmailField(max_length=100, default="")
-    roll_number = models.CharField(max_length=11, default="")
+    roll_number = models.CharField(max_length=12, default="")
     phone_number = models.CharField(max_length=10, default="")
     branch = models.CharField(max_length=4, choices=[('CSE', 'CSE'), ('ECE', 'ECE'), ('EE', 'EE'), ('IT', 'IT'), ('AEIE', 'AEIE')], default='CSE')
     year = models.CharField(max_length=4, default="")
