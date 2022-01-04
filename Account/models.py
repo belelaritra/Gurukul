edd068f38a726f47f4907eaf77086199edd794c5
Soham Banerjee <sohambanerjee4abc@hotmail.com>
2022-01-04 23:04:24 +0530

    minor UI tweaks

--- Account/models.py
+++ Account/models.py
@@ -9,12 +9,21 @@ class Profile(models.Model):
     email = models.EmailField(max_length=100, default="")
     roll_number = models.CharField(max_length=12, default="")
     phone_number = models.CharField(max_length=10, default="")
-    branch = models.CharField(max_length=4, choices=[('CSE', 'CSE'), ('ECE', 'ECE'), ('EE', 'EE'), ('IT', 'IT'), ('AEIE', 'AEIE')], default='CSE')
+    branch = models.CharField(
+        max_length=4,
+        choices=[
+            ("CSE", "CSE"),
+            ("ECE", "ECE"),
+            ("EE", "EE"),
+            ("IT", "IT"),
+            ("AEIE", "AEIE"),
+        ],
+        default="CSE",
+    )
     year = models.CharField(max_length=4, default="")
     auth_token = models.CharField(max_length=100)
     is_verified = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    
-    
+
     def __str__(self):
         return self.user.first_name + " " + self.user.last_name
