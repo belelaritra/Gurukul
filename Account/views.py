25f580c593e8e1d4888a5f4a6bd172c52300c08e
Soham Banerjee <sohambanerjee4abc@hotmail.com>
2022-01-05 00:34:49 +0530

    added landing page

---
+++ Account/views.py
@@ -0,0 +1,220 @@
+from django.shortcuts import redirect, render
+from django.http.response import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.conf import settings
+from django.core.mail import send_mail
+from .models import *
+from Question.models import *
+import uuid
+
+# Create your views here.
+def home(request):
+    return render(request, "Account/home.html")
+
+
+def handleLogin(request):
+    if request.method == "POST":
+        # email=request.POST.get('email')
+        username = request.POST.get("loginusername")
+        password = request.POST.get("loginpassword")
+
+        user_obj = User.objects.filter(username=username).first()
+
+        if user_obj is None:
+            messages.error(request, "User not found")
+            return redirect("/login")
+        else:
+            profile_obj = Profile.objects.filter(user=user_obj).first()
+            if not profile_obj.is_verified:
+                messages.error(
+                    request, "Your account is not verified. Please check your mailbox"
+                )
+                return redirect("/login")
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect("/")
+            else:
+                messages.success(request, "Invalid credentials")
+                return redirect("/login")
+
+    return render(request, "Account/login.html")
+
+
+def handleSignup(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        fname = request.POST["fname"]
+        lname = request.POST["lname"]
+        email = request.POST["email"]
+        pass1 = request.POST["pass1"]
+        pass2 = request.POST["pass2"]
+
+        roll_number = request.POST["roll_number"]
+        phone_number = request.POST["phone_number"]
+        branch = request.POST["branch"]
+        year = request.POST["year"]
+
+        try:
+            # Username
+            if User.objects.filter(username=username).exists():
+                messages.success(request, "Username already exists")
+                return redirect("/signup")
+
+            if not username.isalnum():
+                messages.error(
+                    request, "Username should only contains letters & numbers"
+                )
+                return redirect("/signup")
+
+            if len(username) > 10:
+                messages.error(request, "Username must be under 10 characters")
+                return redirect("/signup")
+
+            # First name
+            if not fname.isalpha():
+                messages.error(request, "first name must contain only characters")
+                return redirect("/signup")
+
+            # Last name
+            if not lname.isalpha():
+                messages.error(request, "Last name must contain only characters")
+                return redirect("/signup")
+
+            # Email
+            if User.objects.filter(email=email).exists():
+                messages.success(request, "Email already exists")
+                return redirect("/signup")
+
+            if not email.endswith("@gmail.com"):
+                messages.error(request, "Email must be a gmail account")
+                return redirect("/signup")
+
+            # Password
+            if pass1 != pass2:
+                messages.error(request, "Passwords do not match")
+                return redirect("/signup")
+            if len(pass1) < 5:
+                messages.error(request, "Password is too short")
+                return redirect("/signup")
+
+            # Create User
+            user_obj = User.objects.create_user(
+                username=username, email=email, password=pass1
+            )
+            user_obj.first_name = fname
+            user_obj.last_name = lname
+            user_obj.save()
+
+            auth_token = str(uuid.uuid4())
+            # Create Profile
+
+            profile_obj = Profile.objects.create(
+                user=user_obj,
+                fname=fname,
+                lname=lname,
+                email=email,
+                roll_number=roll_number,
+                phone_number=phone_number,
+                branch=branch,
+                year=year,
+                auth_token=auth_token,
+            )
+            profile_obj.save()
+
+            send_authentication_mail(username, email, auth_token)
+            # messages.success(request, 'Please check your email to verify your account')
+            return redirect("/token")
+
+        except Exception as e:
+            print(e)
+    # else:
+    #     return HttpResponse('404 - Not Found')
+
+    # print("POST")
+    # print(request.POST)
+    # return render(request, 'Account/success.html')
+    return render(request, "Account/signup.html")
+
+
+def token(request):
+    return render(request, "Account/token.html")
+
+
+def success(request):
+    return render(request, "Account/success.html")
+
+
+def send_authentication_mail(username, email, auth_token):
+    subject = "Verify your Gurukul Account"
+    message = f"Hi {username}, \n\t Thanks for getting started with Gurukul!\nWe need a little more information to complete your registration, including a confirmation of your email address. \n\nClick below to confirm your email address:\n\n{settings.BASE_URL}/verify/{auth_token} \n\nIf you have problems, please paste the above URL into your web browser.\n\nWelcome to Gurukul! \nTeam Gurukul"
+    from_email = settings.EMAIL_HOST_USER
+    to_list = [email]
+    print(
+        f"From: {from_email},\nTo: {to_list},\nSubject: {subject},\nMessage: {message}, "
+    )
+    send_mail(subject, message, from_email, to_list, fail_silently=False)
+
+
+def verify(request, auth_token):
+    try:
+        profile_obj = Profile.objects.filter(auth_token=auth_token).first()
+        if profile_obj:
+            if profile_obj.is_verified:
+                messages.success(request, "Account already verified")
+                return redirect("/login")
+            else:
+                profile_obj.is_verified = True
+                profile_obj.save()
+                messages.success(request, "Your account has been verified successfully")
+                return redirect("/login")
+        else:
+            return redirect("/error")
+    except Exception as e:
+        print(e)
+        messages.success(request, "Something went wrong")
+
+
+def error(request):
+    return render(request, "Account/error.html")
+
+
+# @login_required(login_url='/login')
+def handleLogout(request):
+    # With Proper Authentication
+    if request.method == "GET":
+        logout(request)
+        messages.success(request, "You are now logged out")
+        return redirect("home")
+    # Means Direct Site Access
+    return HttpResponse("404 - Not Found")
+
+
+@login_required(login_url="/login")
+def search(request):
+    query = request.GET["query"]
+    # Very long query
+    if len(query) > 78:
+        allposts = Question.objects.none()
+    # Else
+    else:
+        allposts_Title = Question.objects.filter(
+            title__icontains=query
+        )  # .objects --> Get all the objects from the database
+        allposts_Content = Question.objects.filter(
+            content__icontains=query
+        )  # Content contains Query
+
+        allposts = allposts_Title.union(
+            allposts_Content
+        )  # Union --> Get all the objects from the database
+    if allposts.count() == 0:
+        messages.warning(request, "No search results found. Please refine your search.")
+    params = {"allposts": allposts, "query": query}
+    return render(request, "Account/search.html", params)
+    # return HttpResponse('Search')
--- Gurukul/settings.py
+++ Gurukul/settings.py
@@ -11,16 +11,25 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
+import os
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = BASE_DIR / "templates"
+STATIC_DIR = BASE_DIR / "static"
 
+# Environment variables
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$-&ygwy30+8yhu_!861=0*rv@fbyn41^cq$juv=msbj0#9xkiv'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,52 +40,55 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.humanize",
+    "Account",
+    "Question",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'Gurukul.urls'
+ROOT_URLCONF = "Gurukul.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [TEMPLATES_DIR],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'Gurukul.wsgi.application'
+WSGI_APPLICATION = "Gurukul.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -86,16 +98,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -103,9 +115,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -117,9 +129,29 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Email Send
+
+BASE_URL = "http://127.0.0.1:8000"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_PORT = 587
+
+DEFAULT_FROM_EMAIL = os.environ["EMAIL"]
+SERVER_EMAIL = os.environ["EMAIL"]
+EMAIL_HOST_USER = os.environ["EMAIL"]
+EMAIL_HOST_PASSWORD = os.environ["PASSWORD"]
+
+# Custom Message Tags
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
